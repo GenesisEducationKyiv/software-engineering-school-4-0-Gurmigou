3f2bb0367c148a17df0a52399225eeeaa0189f63
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-06-19 12:43:24 +0300

    [Refactored for OOP]: Refactored model package, implemented utl package

--- main.go
+++ main.go
@@ -1,35 +1,8 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"se-school-case/pkg/controller"
-	"se-school-case/pkg/domain/mail"
-	"se-school-case/pkg/initializer"
-)
-
-func init() {
-	initializer.LoadEnvVariables()
-	initializer.ConnectToDatabase()
-	initializer.RunMigrations()
-	mail.StartScheduledEmail()
-}
+import "se-school-case/api"
 
 func main() {
-	r := gin.Default()
-
-	/*
-		Util end-points:
-		/api/ping - ping-pong server
-		/api/notify - explicitly notifies all users using email without schedules interval
-	*/
-	r.GET("/api/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-	r.POST("/api/notify", controller.PostExplicitlyNotify)
-
-	// Required end-points
-	r.POST("/api/subscribe", controller.PostAddUserEmail)
-	r.GET("/api/rate", controller.GetExchangeRate)
-
-	initializer.StartServer(r)
+	apiService := api.NewApi()
+	apiService.HandleRequests()
 }
---
+++ pkg/util/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+import "time"
+
+const (
+	DefaultCurrentFrom = "USD"
+	DefaultCurrentTo   = "UAH"
+	UpdateInterval     = 1 * time.Hour
+)
