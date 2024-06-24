52a75ab4a1dc906c09cf437a65e878df80c4b9f6
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-06-24 19:00:20 +0300

    [Tests refactored]: Refactored tests in table test manner

--- pkg/domain/mails/mail_integration_test.go
+++ pkg/domain/mails/mail_integration_test.go
@@ -2,66 +2,72 @@ package mails
 
 import (
 	"errors"
+	"testing"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"se-school-case/pkg/domain/rates"
 	"se-school-case/pkg/domain/subscribers"
 	"se-school-case/pkg/model"
 	"se-school-case/pkg/util/constants"
-	"testing"
 )
 
-func TestMailService_SendEmailToAll_Success(t *testing.T) {
-	// Arrange
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockSubscriberService := subscribers.NewMockSubscriberInterface(ctrl)
-	mockRateService := rates.NewMockRateInterface(ctrl)
-
-	expectedUsers := []model.User{
-		{Email: "email1@gmail.com"},
-		{Email: "email2@gmail.com"},
-	}
-	expectedRate := model.Rate{Rate: 27.5}
-	constants.GOOGLE_USERNAME = "se.school.case.2024.notification@gmail.com"
-	constants.GOOGLE_PASSWORD = "password"
-
-	mockSubscriberService.EXPECT().GetAll().Return(expectedUsers, nil)
-	mockRateService.EXPECT().GetRate().Return(expectedRate, nil)
-
-	mailService := NewService(mockSubscriberService, mockRateService)
-
-	// Act
-	err := mailService.SendEmailToAll("Test Subject", "../../util/resource/email.html")
-
-	// Assert
-	assert.NoError(t, err)
+type sendEmailToAllTestCase struct {
+	name          string
+	expectedUsers []model.User
+	expectedRate  model.Rate
+	rateError     error
+	expectedError bool
 }
 
-func TestMailService_SendEmailToAll_RateError(t *testing.T) {
-	// Arrange
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+func TestMailService_SendEmailToAll(t *testing.T) {
+	constants.GOOGLE_USERNAME = "egor555x6@gmail.com"
+	constants.GOOGLE_PASSWORD = "haezjvcfeziujxka"
 
-	mockSubscriberService := subscribers.NewMockSubscriberInterface(ctrl)
-	mockRateService := rates.NewMockRateInterface(ctrl)
-
-	expectedUsers := []model.User{
-		{Email: "email1@gmail.com"},
-		{Email: "email2@gmail.com"},
+	tests := []sendEmailToAllTestCase{
+		{
+			name: "Success",
+			expectedUsers: []model.User{
+				{Email: "email1@gmail.com"},
+				{Email: "email2@gmail.com"},
+			},
+			expectedRate:  model.Rate{Rate: 27.5},
+			expectedError: false,
+		},
+		{
+			name: "RateError",
+			expectedUsers: []model.User{
+				{Email: "email1@gmail.com"},
+				{Email: "email2@gmail.com"},
+			},
+			rateError:     errors.New("internal server error"),
+			expectedError: true,
+		},
 	}
-	constants.GOOGLE_USERNAME = "se.school.case.2024.notification@gmail.com"
-	constants.GOOGLE_PASSWORD = "password"
 
-	mockSubscriberService.EXPECT().GetAll().Return(expectedUsers, nil)
-	mockRateService.EXPECT().GetRate().Return(model.Rate{}, errors.New("internal server error"))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	mailService := NewService(mockSubscriberService, mockRateService)
+			mockSubscriberService := subscribers.NewMockSubscriberInterface(ctrl)
+			mockRateService := rates.NewMockRateInterface(ctrl)
 
-	// Act
-	err := mailService.SendEmailToAll("Test Subject", "../../util/resource/email.html")
+			mockSubscriberService.EXPECT().GetAll().Return(tt.expectedUsers, nil).AnyTimes()
+			mockRateService.EXPECT().GetRate().Return(tt.expectedRate, tt.rateError).AnyTimes()
 
-	// Assert
-	assert.Error(t, err)
+			mailService := NewService(mockSubscriberService, mockRateService)
+
+			// Act
+			err := mailService.SendEmailToAll("Test Subject", "../../util/resource/email.html")
+
+			// Assert
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
--- pkg/domain/rates/rate-fetch_test.go
+++ pkg/domain/rates/rate-fetch_test.go
@@ -8,7 +8,15 @@ import (
 	"testing"
 )
 
-// Create mock fetch server
+type rateFetchTestCase struct {
+	name          string
+	response      string
+	statusCode    int
+	expectedRate  float64
+	expectedError string
+	rateApiUrl    string
+}
+
 func mockRateAPIServer(responseBody string, statusCode int) *httptest.Server {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(statusCode)
@@ -17,58 +25,55 @@ func mockRateAPIServer(responseBody string, statusCode int) *httptest.Server {
 	return httptest.NewServer(handler)
 }
 
-func TestFetchExchangeRate_Success(t *testing.T) {
-	response := `[{"ccy":"USD","base_ccy":"UAH","buy":"36.5686","sale":"37.0700"}]`
-	server := mockRateAPIServer(response, http.StatusOK)
-	defer server.Close()
+func TestFetchExchangeRate(t *testing.T) {
+	tests := []rateFetchTestCase{
+		{
+			name:          "Success",
+			response:      `[{"ccy":"USD","base_ccy":"UAH","buy":"36.5686","sale":"37.0700"}]`,
+			statusCode:    http.StatusOK,
+			expectedRate:  37.0700,
+			expectedError: "",
+		},
+		{
+			name:          "ErrorFetching",
+			rateApiUrl:    "http://invalid-url",
+			expectedError: "error fetching exchange rates",
+		},
+		{
+			name:          "ErrorUnmarshaling",
+			response:      `invalid json`,
+			statusCode:    http.StatusOK,
+			expectedError: "error unmarshaling response",
+		},
+		{
+			name:          "RateNotFound",
+			response:      `[{"ccy":"EUR","base_ccy":"UAH","buy":"40.0000","sale":"41.0000"}]`,
+			statusCode:    http.StatusOK,
+			expectedError: "rates not found",
+		},
+	}
 
-	constants.RATE_API_URL = server.URL
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup mock server if response and statusCode are provided
+			if tt.response != "" {
+				server := mockRateAPIServer(tt.response, tt.statusCode)
+				defer server.Close()
+				constants.RATE_API_URL = server.URL
+			} else {
+				constants.RATE_API_URL = tt.rateApiUrl
+			}
 
-	service := NewRateFetchService()
-	rate, err := service.FetchExchangeRate()
+			service := NewRateFetchService()
+			rate, err := service.FetchExchangeRate()
 
-	assert.NoError(t, err)
-	assert.Equal(t, 37.0700, rate)
-}
-
-// Test error if fetching from server with invalid url
-func TestFetchExchangeRate_ErrorFetching(t *testing.T) {
-	constants.RATE_API_URL = "http://invalid-url"
-
-	service := NewRateFetchService()
-	_, err := service.FetchExchangeRate()
-
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "error fetching exchange rates")
-}
-
-// Test unmarshling error if mock fetch server return invalid json
-func TestFetchExchangeRate_ErrorUnmarshaling(t *testing.T) {
-	response := `invalid json`
-	server := mockRateAPIServer(response, http.StatusOK)
-	defer server.Close()
-
-	constants.RATE_API_URL = server.URL
-
-	service := NewRateFetchService()
-	_, err := service.FetchExchangeRate()
-
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "error unmarshaling response")
-}
-
-// Mock fetch server responds with exchange rate EUR to UAH, not USD to UAH
-// (which are the default values of constants.DefaultCurrentFrom and constants.DefaultCurrentTo)
-func TestFetchExchangeRate_RateNotFound(t *testing.T) {
-	response := `[{"ccy":"EUR","base_ccy":"UAH","buy":"40.0000","sale":"41.0000"}]`
-	server := mockRateAPIServer(response, http.StatusOK)
-	defer server.Close()
-
-	constants.RATE_API_URL = server.URL
-
-	service := NewRateFetchService()
-	_, err := service.FetchExchangeRate()
-
-	assert.Error(t, err)
-	assert.Equal(t, "rates not found", err.Error())
+			if tt.expectedError == "" {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedRate, rate)
+			} else {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tt.expectedError)
+			}
+		})
+	}
 }
