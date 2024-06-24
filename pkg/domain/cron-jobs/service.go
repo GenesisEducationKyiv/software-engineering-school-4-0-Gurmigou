6d9bbefbd7113732982d216faa5d02cb58e44bf5
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-06-24 19:02:46 +0300

    [Fixed]: Fixed

--- pkg/domain/cron-jobs/service.go
+++ pkg/domain/cron-jobs/service.go
@@ -20,8 +20,11 @@ func (s *CronJobsService) StartScheduler() {
 
 	_, err := scheduler.Every(1).Day().At(
 		constants.EMAIL_SEND_TIME).Do(func() {
-		s.mailService.SendEmailToAll(
+		err := s.mailService.SendEmailToAll(
 			"Exchange rates notification", constants.TEMPLATE_PATH)
+		if err != nil {
+			return
+		}
 	})
 	if err != nil {
 		log.Fatalf("Error scheduling email notifications: %v", err)
