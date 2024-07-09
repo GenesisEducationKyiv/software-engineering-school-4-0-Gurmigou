0bc241d01804cf78fcff7e39d4a4c915b2ee45e0
Yehor Bukhanets <43072571+Gurmigou@users.noreply.github.com>
2024-07-09 13:49:47 +0300

    Hw #5 application architecture (#6)
    * [Updated deps]: Updated dependency versions, installed library for dependency tests
    
    * [Architecture]: Implemented architecture schema
    
    * [Architecture]: Implemented arch-go.yml configuration, added architecture_test.go
    
    * [Architecture]: Added configuration for lint.yml to check architecture dependencies
    
    ---------
    
    Co-authored-by: YehorBukhane <yehor.bukhanets@software-aspekte.de>

---
+++ docs/architecture_test.go
@@ -0,0 +1,86 @@
+package docs
+
+import (
+	"testing"
+
+	archgo "github.com/fdaines/arch-go/api"
+	config "github.com/fdaines/arch-go/api/configuration"
+)
+
+func TestArchitecture(t *testing.T) {
+	configuration := config.Config{
+		DependenciesRules: []*config.DependenciesRule{
+			{
+				Package: "cmd/myapp",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"initializer"},
+				},
+			},
+			{
+				Package: "initializer",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"internal", "pkg", "db"},
+				},
+			},
+			{
+				Package: "db",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"pkg"},
+				},
+			},
+			{
+				Package: "internal/cron-jobs",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"internal", "pkg/constants"},
+				},
+			},
+			{
+				Package: "internal/external-api/rate",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"pkg/constants", "pkg/util"},
+				},
+			},
+			{
+				Package: "internal/mails",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{
+						"internal/cron-jobs", "internal/rates", "internal/subscribers",
+						"pkg/constants", "pkg/util", "pkg/model",
+					},
+				},
+			},
+			{
+				Package: "internal/rates",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"pkg/model", "pkg/constants", "internal/external-api/rate"},
+				},
+			},
+			{
+				Package: "internal/subscribers",
+				ShouldOnlyDependsOn: &config.Dependencies{
+					Internal: []string{"pkg/model"},
+				},
+			},
+			{
+				Package:             "pkg/constants",
+				ShouldOnlyDependsOn: &config.Dependencies{},
+			},
+			{
+				Package:             "pkg/model",
+				ShouldOnlyDependsOn: &config.Dependencies{},
+			},
+			{
+				Package:             "pkg/util",
+				ShouldOnlyDependsOn: &config.Dependencies{},
+			},
+		},
+	}
+
+	moduleInfo := config.Load("github.com/Gurmigou/se-school-case-2024")
+
+	result := archgo.CheckArchitecture(moduleInfo, configuration)
+
+	if !result.Passes {
+		t.Fatal("Architecture tests failed!")
+	}
+}
