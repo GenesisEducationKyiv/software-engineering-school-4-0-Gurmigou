021413839212aa1de18725fe390cf0811d9bfaf5
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-07-10 22:33:47 +0300

    [Fix]: Fixed architecture_test.go

--- docs/architecture_test.go
+++ docs/architecture_test.go
@@ -56,6 +56,7 @@ func TestArchitecture(t *testing.T) {
 				},
 			},
 			{
+				Package: "internal/subscriber",
 				ShouldOnlyDependsOn: &config.Dependencies{
 					Internal: []string{"pkg/model"},
 				},
