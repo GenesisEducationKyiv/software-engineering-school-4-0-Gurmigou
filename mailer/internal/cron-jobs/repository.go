1452c4ae7015009124cfa236a27f397f3eca9324
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-07-22 23:37:41 +0300

    [Impl]: Implemented repository.go for mailer service

---
+++ mailer/internal/cron-jobs/repository.go
@@ -0,0 +1,67 @@
+package cron_jobs
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Email string `gorm:"uniqueIndex;not null"`
+}
+
+type Repository interface {
+	Create(subscriber *User) error
+	FindByEmail(email string) (*User, error)
+	FindAll() ([]User, error)
+	DeleteOne(subscriber *User) error
+	DeleteAll() error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) Create(subscriber *User) error {
+	if err := r.db.Create(subscriber).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) FindByEmail(email string) (*User, error) {
+	var user User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *repository) FindAll() ([]User, error) {
+	var users []User
+	if err := r.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (r *repository) DeleteOne(subscriber *User) error {
+	if err := r.db.Delete(subscriber).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *repository) DeleteAll() error {
+	if err := r.db.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&User{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
